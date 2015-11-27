304160a93132469d1d72e8afd003920c61d42620
Ben Green <ben.green@inventati.org>
2015-11-26 20:42:27 -0500

    update wsgi

--- ReelioChallenge/wsgi.py
+++ ReelioChallenge/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import dj_static
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReelioChallenge.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
